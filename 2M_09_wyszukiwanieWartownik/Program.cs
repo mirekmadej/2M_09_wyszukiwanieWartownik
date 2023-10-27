61bcccef758a09400dd571e8e5f6e30cbfe7307e
Mirek Madej <m.madej@zsp3zamosc.pl>
2023-10-27 10:10:50 +0200

    Dodaj pliki projektów.

---
+++ 2M_09_wyszukiwanieWartownik/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _2M_09_wyszukiwanieWartownik
+{
+    class Wyszukiwanie
+    {
+        private const int N = 50;
+        private int[] liczby = new int[N+1];
+        private int szukana;
+        public Wyszukiwanie()
+        {
+            generujDane();
+        }
+        private void generujDane()
+        {
+            Random random = new Random();
+            for (int i = 0; i < N; i++)
+                liczby[i] = random.Next(1,100);
+            liczby[N] = szukana;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
